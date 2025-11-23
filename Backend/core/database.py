c5c817b8a8dd7acb259f3e0620a9d66438709242
Thy Tran <58045538+ThyTran1402@users.noreply.github.com>
2025-11-22 21:59:44 -0500

    setup ci/cd

--- Backend/core/database.py
+++ Backend/core/database.py
@@ -2,11 +2,16 @@ import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
+from contextlib import contextmanager
 
 load_dotenv()
 
+@contextmanager
 def get_db():
-    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+    conn = psycopg2.connect(
+        os.getenv("DATABASE_URL"),
+        cursor_factory=RealDictCursor
+    )
     try:
         yield conn
     finally:
